be8f356f4575b7bb01ae717c0fbaa452ce34ade9
juyonl <123980343+juyonl@users.noreply.github.com>
2024-07-17 05:34:53 +0300

    Adding py files

---
+++ header.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def create_header():
+  st.title("Inspirit AI: Heart Disease Model")
+  st.header("By InspiritAI")
+  st.subheader("Given your blood pressure and heart rate, let's predict if you have heart disease!")
