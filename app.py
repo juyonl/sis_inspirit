5e8e55e7e0a3da5c84bff78265e86a630ef462d0
juyonl <123980343+juyonl@users.noreply.github.com>
2024-07-18 16:07:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from joblib import load
+import numpy as np 
 import os
+import sklearn
 
 # Imports the functions we coded above
 from header import *
@@ -9,8 +11,9 @@ from response import *
 from predictor import *
 
 # Load our DecisionTree model into our web app
-print("DEBUG")
-print(os.getcwd())
+print(numpy.__version__)
+print(joblib.__version__)
+print(sklearn.__version__)
 model = load("model.joblib")
 st.write ("Model uploaded!") # You may remove this in your finalized web app!
 
